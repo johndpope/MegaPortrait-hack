f58520c0da57e6fb8f9f01de383b51b203903d0a
John Pope <jp@bellgeorge.com>
2024-05-13 14:43:45 +1000

    ok

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from resnet import ResNet,Bottleneck, resnet18,ResBlock2D,ResNet18
+from resnet import ResNet,Bottleneck, resnet18
 import torchvision.models as models
 
 
@@ -402,7 +402,47 @@ class G3d(nn.Module):
         return x
 
 
-
+class ResBlock2D(nn.Module):
+    def __init__(self, in_channels, out_channels, downsample=False):
+        super(ResBlock2D, self).__init__()
+        self.downsample = downsample
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        
+        if self.downsample:
+            self.downsample_conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=2)
+            self.downsample_bn = nn.BatchNorm2d(out_channels)
+        
+        if in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1),
+                nn.BatchNorm2d(out_channels)
+            )
+        else:
+            self.shortcut = nn.Identity()
+        
+    def forward(self, x):
+        identity = x
+        
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = nn.ReLU(inplace=True)(out)
+        
+        out = self.conv2(out)
+        out = self.bn2(out)
+        
+        if self.downsample:
+            identity = self.downsample_conv(x)
+            identity = self.downsample_bn(identity)
+        
+        identity = self.shortcut(identity)
+        
+        out += identity
+        out = nn.ReLU(inplace=True)(out)
+        
+        return out
 '''
 The G2d class consists of the following components:
 
@@ -918,6 +958,75 @@ The decoder consists of custom residual blocks, upsampling operations, and a fin
 
 GDT
 '''
+class ResBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, downsample=False):
+        super().__init__()
+        if downsample:
+            self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1)
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=2),
+                nn.BatchNorm2d(out_channels)
+            )
+        else:
+            self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+            self.shortcut = nn.Sequential()
+
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+    def forward(self, input):
+        shortcut = self.shortcut(input)
+        input = nn.ReLU()(self.bn1(self.conv1(input)))
+        input = nn.ReLU()(self.bn2(self.conv2(input)))
+        input = input + shortcut
+        return nn.ReLU()(input)
+
+
+class ResNet18(nn.Module):
+    def __init__(self, in_channels=3, num_classes=1000):
+        super().__init__()
+        self.layer0 = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        self.layer1 = nn.Sequential(
+            ResBlock(64, 64),
+            ResBlock(64, 64)
+        )
+
+        self.layer2 = nn.Sequential(
+            ResBlock(64, 128, downsample=True),
+            ResBlock(128, 128)
+        )
+
+        self.layer3 = nn.Sequential(
+            ResBlock(128, 256, downsample=True),
+            ResBlock(256, 256)
+        )
+
+        self.layer4 = nn.Sequential(
+            ResBlock(256, 512, downsample=True),
+            ResBlock(512, 512)
+        )
+
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, num_classes)
+
+    def forward(self, input):
+        input = self.layer0(input)
+        input = self.layer1(input)
+        input = self.layer2(input)
+        input = self.layer3(input)
+        input = self.layer4(input)
+        input = self.avg_pool(input)
+        input = input.view(input.size(0), -1)
+        input = self.fc(input)
+        return input
+    
 
 class Student(nn.Module):
     def __init__(self, num_avatars):
