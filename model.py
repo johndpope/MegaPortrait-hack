3eab2f1ab7fdf30a604517b248c2f71c0d3c680b
John Pope <jp@bellgeorge.com>
2024-06-13 20:41:10 +1000

    ok

--- model.py
+++ model.py
@@ -184,96 +184,50 @@ class Eapp(nn.Module):
     def __init__(self):
         super().__init__()
         
-          # First part: producing volumetric features vs
-        self.conv = nn.Conv2d(3, 64, 7, stride=1, padding=3).to(device)
-        self.resblock_128 = ResBlock_Custom(dimension=2, in_channels=64, out_channels=128).to(device)
-        self.resblock_256 = ResBlock_Custom(dimension=2, in_channels=128, out_channels=256).to(device)
-        self.resblock_512 = ResBlock_Custom(dimension=2, in_channels=256, out_channels=512).to(device)
-
-        # round 0
+        # First part: producing volumetric features vs
+        self.hourglass = Hourglass(block_expansion=64, in_features=3, num_blocks=4, max_features=512)
+        self.conv1x1 = nn.Conv2d(self.hourglass.out_filters, 96, kernel_size=1)
+        
+        # 3D ResBlock layers
         self.resblock3D_96 = ResBlock3D_Adaptive(in_channels=96, out_channels=96).to(device)
         self.resblock3D_96_2 = ResBlock3D_Adaptive(in_channels=96, out_channels=96).to(device)
-
-        # round 1
         self.resblock3D_96_1 = ResBlock3D_Adaptive(in_channels=96, out_channels=96).to(device)
         self.resblock3D_96_1_2 = ResBlock3D_Adaptive(in_channels=96, out_channels=96).to(device)
-
-        # round 2
         self.resblock3D_96_2 = ResBlock3D_Adaptive(in_channels=96, out_channels=96).to(device)
         self.resblock3D_96_2_2 = ResBlock3D_Adaptive(in_channels=96, out_channels=96).to(device)
 
-        self.conv_1 = nn.Conv2d(in_channels=512, out_channels=1536, kernel_size=1, stride=1, padding=0).to(device)
-
-        # Adjusted AvgPool to reduce spatial dimensions effectively
-        self.avgpool = nn.AvgPool2d(kernel_size=2, stride=2, padding=0).to(device)
-
         # Second part: producing global descriptor es
-        self.custom_resnet50 = CustomResNet50().to(device)
-        '''
-        ### TODO 2: Change vs/es here for vector size
-        According to the description of the paper (Page11: predict the head pose and expression vector), 
-        zs should be a global descriptor, which is a vector. Otherwise, the existence of Emtn and Eapp is of little significance. 
-        The output feature is a matrix, which means it is basically not compressed. This encoder can be completely replaced by a VAE.
-        '''        
-        filters = [64, 256, 512, 1024, 2048]
-        outputs=COMPRESS_DIM
-        self.fc = torch.nn.Linear(filters[4], outputs)       
+        self.fc = torch.nn.Linear(512, COMPRESS_DIM)
        
     def forward(self, x):
         # First part
-        logging.debug(f"image x: {x.shape}") # [1, 3, 256, 256]
-        out = self.conv(x)
-        logging.debug(f"After conv: {out.shape}")  # [1, 3, 256, 256]
-        out = self.resblock_128(out)
-        logging.debug(f"After resblock_128: {out.shape}") # [1, 128, 256, 256]
-        out = self.avgpool(out)
-        logging.debug(f"After avgpool: {out.shape}")
+        logging.debug(f"image x: {x.shape}")  # [1, 3, 256, 256]
         
-        out = self.resblock_256(out)
-        logging.debug(f"After resblock_256: {out.shape}")
-        out = self.avgpool(out)
-        logging.debug(f"After avgpool: {out.shape}")
+        feature_map = self.hourglass(x)
+        logging.debug(f"After hourglass: {feature_map.shape}")  # [1, 512, 64, 64]
         
-        out = self.resblock_512(out)
-        logging.debug(f"After resblock_512: {out.shape}") # [1, 512, 64, 64]
-        out = self.avgpool(out) # at 512x512 image training - we need this  🤷 i rip this out so we can keep things 64x64 - it doesnt align to diagram though
-        # logging.debug(f"After avgpool: {out.shape}") # [1, 256, 64, 64]
-   
-        out = F.group_norm(out, num_groups=32)
-        out = F.relu(out)
-        out = self.conv_1(out)
-        logging.debug(f"After conv_1: {out.shape}") # [1, 1536, 32, 32]
+        vs = self.conv1x1(feature_map)
+        logging.debug(f"After conv1x1: {vs.shape}")  # [1, 96, 64, 64]
         
-     # reshape 1546 -> C96 x D16
-        vs = out.view(out.size(0), 96, 16, *out.shape[2:]) # 🤷 this maybe inaccurate
-        logging.debug(f"reshape 1546 -> C96 x D16 : {vs.shape}") 
+        vs = vs.view(vs.size(0), 96, 16, *vs.shape[2:])
+        logging.debug(f"reshape to C96 x D16: {vs.shape}")  # [1, 96, 16, 64, 64]
         
-        
-        # 1
+        # Apply 3D ResBlock layers
         vs = self.resblock3D_96(vs)
-        logging.debug(f"After resblock3D_96: {vs.shape}") 
         vs = self.resblock3D_96_2(vs)
-        logging.debug(f"After resblock3D_96_2: {vs.shape}") # [1, 96, 16, 32, 32]
-
-        # 2
         vs = self.resblock3D_96_1(vs)
-        logging.debug(f"After resblock3D_96_1: {vs.shape}") # [1, 96, 16, 32, 32]
         vs = self.resblock3D_96_1_2(vs)
-        logging.debug(f"After resblock3D_96_1_2: {vs.shape}")
-
-        # 3
         vs = self.resblock3D_96_2(vs)
-        logging.debug(f"After resblock3D_96_2: {vs.shape}") # [1, 96, 16, 32, 32]
         vs = self.resblock3D_96_2_2(vs)
-        logging.debug(f"After resblock3D_96_2_2: {vs.shape}")
+        logging.debug(f"After 3D ResBlocks: {vs.shape}")  # [1, 96, 16, 64, 64]
 
         # Second part
-        es_resnet = self.custom_resnet50(x)
-        ### TODO 2
-        # print(f"🍌 es:{es_resnet.shape}") # [1, 512, 2, 2]
-        es_flatten = torch.flatten(es_resnet, start_dim=1)
-        es = self.fc(es_flatten) # torch.Size([bs, 2048]) -> torch.Size([bs, 2])        
-        print(f"🍌 es:{es.shape} vs{vs.shape}")
+        es = F.adaptive_avg_pool2d(feature_map, (1, 1)).view(x.size(0), -1)
+        logging.debug(f"Adaptive avg pool: {es.shape}")  # [1, 512]
+        
+        es = self.fc(es)
+        logging.debug(f"Final es shape: {es.shape}")  # [1, COMPRESS_DIM]
+       
         return vs, es
 
 
